9bbf06281ff5f20b237391ed4b0e81f4b57791c2
Antonio <antonio.ticante@erpp.mx>
2024-04-16 13:52:04 -0600

    Todos los campos al momento de crear al usuario

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -8,20 +8,37 @@ export class RegisterUserDto {
         public password: string,
         public nombre: string,
         public apellido_paterno: string,
+        public apellido_materno: string,
+        public url_foto: string,
+        public access_app_movil: number,
+        public access_app_web: number,
+        public sexo: string,
+        public activo: number,
+        public telefono_personal: string,
+        public id_user_push: string,
+        public correo: string
     ) { }
 
     static create( object: { [key: string]: any } ): [string?, RegisterUserDto?] {
-
-        const { usuario, password, nombre, apellido_paterno } = object;
+        
+        let { usuario, password, nombre, apellido_paterno, apellido_materno, url_foto, access_app_web, access_app_movil, sexo, activo, telefono_personal, id_user_push, correo } = object;
 
         if( !usuario ) return ['Missing usuario'];
-        if( !regularExps.email.test( usuario ) ) return ['Email is not valid']
-        if( !password ) return ['Missing password']
-        if( !nombre ) return ['Missing nombre']
-        if( !apellido_paterno ) return ['Missing apellido paterno']
+        if( !regularExps.email.test( usuario ) ) return ['Email is not valid'];
+        if( !password ) return ['Missing password'];
+        if( !nombre ) return ['Missing nombre'];
+        if( !apellido_paterno ) return ['Missing apellido paterno'];
+        if( !apellido_materno ) apellido_materno = ''
+        if( !url_foto ) url_foto = 'https://firebasestorage.googleapis.com/v0/b/waterloo-6e309.appspot.com/o/images%2Fusuarios%2FUsuario-Base-Demo?alt=media&token=1295b4a8-7a70-4aeb-9507-5e8c63f2abc0';
+        if( !access_app_web ) access_app_web = 1;
+        if( !access_app_movil ) access_app_movil = 1;
+        if( !sexo ) sexo = ''
+        if( !telefono_personal ) telefono_personal = ''
+        if( !id_user_push ) id_user_push = ''
+        if( !activo ) activo = 1
+        if( !correo ) correo = ''
         
-
-        return [undefined, new RegisterUserDto(usuario, password, nombre, apellido_paterno)]
+        return [undefined, new RegisterUserDto(usuario, password, nombre, apellido_paterno, apellido_materno, url_foto, access_app_web, access_app_movil, sexo, activo, telefono_personal, id_user_push, correo)]
 
     }
 
